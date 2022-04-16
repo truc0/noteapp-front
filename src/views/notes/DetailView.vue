552a0bbe2e6c11c0b9dec5f2b1d1fad7d6ce9f94
truc0 <22969604+truc0@users.noreply.github.com>
2022-04-16 21:53:22 +0800

    feat(notes): add delete function

--- src/views/notes/DetailView.vue
+++ src/views/notes/DetailView.vue
@@ -11,6 +11,7 @@
                 class="rounded px-2 py-1 transition-all
                         text-red-400 hover:text-red-600"
                 title="Delete"
+                @click="deleteNote"
             >
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
@@ -49,6 +50,21 @@ export default {
             }).then(response => {
                 this.title = response.data.title
                 this.content = response.data.content
+            })
+        },
+        deleteNote() {
+            if (!confirm('Are you sure you want to delete this?')) { return }
+
+            axios.delete(`${URL_BASE}/notes/${this.id}`, {
+                headers: {
+                    Authorization: `Token ${this.token}`
+                }
+            }).then(response => {
+                if (response.status === 204) {
+                    this.$router.push({ name: 'home' })
+                } else {
+                    console.log(response)
+                }
             })
         }
     },
